4fcdd88e045499532a20679f1920ea7e1316831b
Gerald McAlister <gerald.mcalister.gem@gmail.com>
2013-05-14 00:42:29 -0400

    Updated Texture Management
    -Updated texture management system.  Textures must be kept track of now
    when added!!!

--- The Game/The Game/BaseObjectClass.cpp
+++ The Game/The Game/BaseObjectClass.cpp
@@ -13,26 +13,20 @@ BaseObject::BaseObject(const string filename)
 
 void BaseObject::addTexture(const string filename)
 {
-	if(BaseObject::textureList.count(filename) > 0)
-	{
-		string s(filename.c_str());
-		s.append(to_string((long long)BaseObject::textureList.size()));
-		BaseObject::textureList.insert(pair<string, Texture*>(s, BaseObject::textureList.at(filename)));
-		this->filename = s;
-	}
-	else
+	if(BaseObject::textureList.count(filename) == 0)
 	{
 		Texture* t = new Texture();
 		if(!t->loadFromFile(filename))
 		{
 			cout << "Could not load texture " << filename << endl;
+			return;
 		}
 		else
 		{
 			BaseObject::textureList.insert(std::pair<std::string, Texture*>(filename, t));
-			this->filename = filename;
 		}
 	}
+	this->filename = filename;
 }
 
 void BaseObject::addTextureToList(const string filename)
@@ -55,12 +49,12 @@ void BaseObject::removeTextureFromList(const string filename)
 {
 	if(BaseObject::textureList.count(filename) > 0)
 	{
+		BaseObject::textureList.at(filename)->~Texture();
 		BaseObject::textureList.erase(filename);
 	}
 }
 
 BaseObject::~BaseObject()
 {
-	BaseObject::textureList.erase(this->filename);
 }
 
